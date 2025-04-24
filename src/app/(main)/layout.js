12f6a8bcafa992cc9cace446760e3c5df6cac42f
Ani Medulashvili <animedulashvili12@gmail.com>
2025-04-24 21:28:34 +0400

    add profile information

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,12 +1,84 @@
-import styles from './page.module.css'
+// 'use client'
+// import Link from 'next/link';
+// import styles from './page.module.css'
+// import { usePathname } from 'next/navigation'
 
-function layout ( {children} ) {
-    return (
-        <div className={styles.container}>
-            <nav className={styles.nav}>123</nav>
-            {children}
-        </div>
-    )
-};
+// function layout ( {children} ) {
+//     const pathname = usePathname();
+
+
+//     return (
+//         <div className={styles.container}>
+//             <nav className={styles.nav}>
+//                 <Link 
+//                 className={`
+//                     ${styles.Link}
+//                     ${pathname === "/products" ? styles.activeLink : styles.inActiveLink}
+//                 `} 
+//                 href="/products"
+//                 >
+//                     Products
+//                     </Link>
+//                 <Link
+//                 className={`
+//                     ${styles.Link}
+//                     ${pathname === "/profile" ? styles.activeLink : styles.inActiveLink}
+//                  `} 
+//                  href="/profile"
+//                  >
+//                     Profile
+//                     </Link>
+//             </nav>
+//             {children}
+//         </div>
+//     )
+// };
+
+// export default layout;
+
+
+
+
+
+
+
+"use client";
+import Link from "next/link";
+import styles from "./page.module.css";
+import { usePathname } from "next/navigation";
+
+function layout({ children }) {
+  const pathname = usePathname();
+
+  console.log('client')
+
+  return (
+    <div className={styles.container}>
+      <nav className={styles.nav}>
+        <Link
+          className={`
+            ${styles.link}
+            ${
+              pathname.includes("/products") ? styles.activeLink : styles.inActiveLink
+            }`}
+          href="/products"
+        >
+          Products
+        </Link>
+        <Link
+          className={`
+            ${styles.link}
+            ${pathname.includes("/profile") ? styles.activeLink : styles.inActiveLink}
+            `}
+          href="/profile"
+        >
+          Profile
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
+}
 
 export default layout;
+
--- src/app/(main)/profile/page.jsx
+++ src/app/(main)/profile/page.jsx
@@ -36,7 +36,39 @@ const Profile = async () => {
             <p>გვარი:</p>
             <p>{profile.lastName}</p>
         </div>
-     
+        <div className={styles.infoWrapper}>
+            <p>ტელეფონი:</p>
+            <p>{profile.phone}</p>
+        </div>
+        <div className={styles.infoWrapper}>
+            <p>მეილი:</p>
+            <p>{profile.email}</p>
+        </div>
+        <div className={styles.infoWrapper}>
+            <p>დაბადების თარიღი:</p>
+            <p>{profile.birthDate}</p>
+        </div>
+        <div className={styles.infoWrapper}>
+            <p>მისამართი:</p>
+            <p>{profile.address.country};</p>
+            <p>{profile.address.state};</p>  
+            <p>{profile.address.address}</p>
+        </div>
+        <div className={styles.infoWrapper}>
+            <p>უნივერსიტეტი:</p>
+            <p>{profile.university}</p>
+        </div>
+        <div className={styles.infoWrapper}>
+            <p>კომპანია:</p>
+            <p>{profile.company.name}</p>
+        </div>
+        <div className={styles.infoWrapper}>
+            <p>კომპანიის მისმართი:</p>
+            <p>{profile.company.address.city};</p>
+            <p>{profile.company.address.state};</p>
+            <p>{profile.company.address.address}</p>
+        </div>
+        
         </section>
     </div>
     
